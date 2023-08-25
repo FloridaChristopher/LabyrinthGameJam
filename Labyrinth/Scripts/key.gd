0f6f4eff523d221d803e2ff5a922da86196935d4
Woodhouse3D <45299674+FloridaChristopher@users.noreply.github.com>
2023-08-25 18:54:59 -0400

    added key sound

--- Labyrinth/Scripts/key.gd
+++ Labyrinth/Scripts/key.gd
@@ -4,6 +4,8 @@ extends Area3D
 @export var isBiggerKey: bool = false
 @export var id: int = 0
 
+@onready var mesh = $Node3D
+@export var audio: AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,5 +21,12 @@ func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		get_tree().get_nodes_in_group("UI").pop_front().add_keys(1,isBiggerKey)
 		get_tree().get_nodes_in_group("Control").pop_front().got_key(id)
-		queue_free()
+		mesh.visible = false
+		audio.play()
+		#queue_free()
+	pass # Replace with function body.
+
+
+func _on_audio_stream_player_finished():
+	queue_free()
 	pass # Replace with function body.
